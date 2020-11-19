9e1d648fe1827d020c7aabfa0e503cbbce63fd18
Julio Cesar Palacios Torres <150300156@ucaribe.edu.mx>
2020-11-18 23:44:41 -0500

    modificacion del header y index

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,42 +1,34 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
+import "./index.css";
 
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <Link to="/" className="navbar-brand">
-            UCARIBE
-          </Link>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link to="/" className="nav-link">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/add" className="nav-link">
-                  crear reporte
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/seeReports" className="nav-link">
-                  ver reportes
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/login" className="nav-link">
-                  Login
-                </Link>
-              </li>
-            </ul>
+      <>
+        <header className="header">
+          <div className="container logo-nav-container">
+            <a>LOGO</a>
+            <nav className="navigation">
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/add">Crear Reporte</Link>
+                </li>
+                <li>
+                  <Link to="/seeReports">Ver Reportes</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+              </ul>
+            </nav>
           </div>
-        </nav>
-      </div>
+        </header>
+      </>
     );
   }
 }
